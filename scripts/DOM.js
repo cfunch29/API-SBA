9d652b6a69aeca6d6d6828464edd7a7c432b5aca
cfunch29 <cfunch29@gmail.com>
2026-01-19 23:07:23 -0500

    added css to center results

--- scripts/DOM.js
+++ scripts/DOM.js
@@ -17,8 +17,21 @@ export function renderRecipes(meals){
         //bootstrap styling of card format for meal results with img and directions with ingredients
         card.innerHTML = 
         `<div class = "row g-0">
-        <div class = "col-md4">
+        <div class = "col-md-4">
         <img src="${meal.strMealThumb}" class="img-fluid rounded-start" alt="${meal.strMeal}">
+        </div>
+        <div class ="col-md-8">
+        <div class ="card-body">
+        <h5 class ="card-title">${meal.strMeal}</h5>
+        <p class ="card-text">
+        <strong>Category:</strong> ${meal.strCategory}<br>
+        <strong>Area:</strong> ${meal.strArea}
+        </p>
+
+        <h6>Instructions</h6>
+        <p class ="card-text">
+        ${meal.strInstructions}
+        </p>
         </div>
         </div>
         </div>`;
