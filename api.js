c43c8541b3b31c86c63e90a38a6e212326a7c707
cfunch29 <cfunch29@gmail.com>
2026-01-19 21:35:36 -0500

    updates api javascript file with async function and GET request also set up first module

--- api.js
+++ api.js
@@ -1,5 +1,30 @@
-export async function fetchRecipes(searchTerm {
-    
-}
+let apiURL = `www.themealdb.com/api/json/v1/1/search.php?s=${searchMeal}`;
 
-let apiURL = "www.themealdb.com/api/json/v1/1/search.php?s=";
+export async function fetchRecipes(searchMeal) {
+    try {
+        // try searching by meal name 
+        let response = await fetch(apiURL, {
+            method: "GET"
+        });
+
+        if (response.status < 200 || response.status >= 300) {
+            throw new Error (`HTTP Error Status: ${response.status}`);
+        }
+           // if meals are found by name, return meal
+        let data = await response.json();
+
+            if (data.meals){
+                return data.meals;
+            }
+ 
+        }
+
+    catch (err) {
+        console.error("Error fetching recipes", err);
+            return [];
+    }
+};
+
+        
+
+
