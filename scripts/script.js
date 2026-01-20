f4963623168e16b1abb05646a15cc1b31334ae2d
cfunch29 <cfunch29@gmail.com>
2026-01-19 21:45:10 -0500

    created results element in script

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,8 @@
-let apiURL = "www.themealdb.com/api/json/v1/1/search.php?s=";
+// let apiURL = "www.themealdb.com/api/json/v1/1/search.php?s=";
+
+
+const resultsDiv = document.createElement("div");
+    resultsDiv.id = "results";
+    resultsDiv.class = "container mt-4";
+    document.body.appendChild(resultsDiv);
+    
